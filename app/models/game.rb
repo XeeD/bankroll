93abab1c46798d712b1146f44849a617e5dc38e5
Lukáš Voda <xeedus@gmail.com>
2013-12-22 09:23:29 +0100

    Refactor Game to use more of Ruby´s power

--- app/models/game.rb
+++ app/models/game.rb
@@ -12,31 +12,30 @@ class Game < ActiveRecord::Base
   end
 
   def self.import(file, user)
-    workbook = create_workbook file
-    workbook.each do |param|
-      @game = Game.new
-      @game.buyin = param[0] || 0
-      @game.prize = param[1] || 0
-      @game.variant_id = param[2] || 0
-      @game.casino_id = param[3] || 0
-      @game.user_id = user.id
-      @game.save
+    workbook(file).map do |line|  
+      create(
+        buyin: line[0].to_i
+        prize: line[1].to_i
+        variant_id: line[2].to_i
+        casino_id: line[3].to_i
+        user_id: user.id
+      )
     end
   end
 
-  def self.create_workbook(file)
+  def self.workbook(file)
     workbook = {}
     extension = File.extname(file.original_filename)
 
-    if extension == ".xls"
-      workbook = Roo::Excel.new(file.path, file_warning: :ignore)
-    elsif extension == ".xlsx"
-      workbook = Roo::Excelx.new(file.path, file_warning: :ignore)
-    elsif extension == ".csv"
-      workbook = Roo::Csv.new(file.path, file_warning: :ignore)
+    case extension
+    when ".xls"
+      Roo::Excel.new(file.path, file_warning: :ignore)
+     when ".xlsx"
+      Roo::Excelx.new(file.path, file_warning: :ignore)
+    when == ".csv"
+      Roo::Csv.new(file.path, file_warning: :ignore)
     else
       raise "File type was not recognised."
     end
-    workbook
   end
 end
